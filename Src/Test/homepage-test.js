85af944266af4fb154b2fdae921231da435a116f
vamseekakani <58133158+vamseekakani@users.noreply.github.com>
2020-12-18 22:15:12 +0530

    Delete homepage-test.js

--- Src/Test/homepage-test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-
-describe("Test webdriveruni homepage", () => {
-  it("Validate whether the webdriver uni homepage title is correct", () => {
-      browser.url('www.webdriveruniversity.com')
-      const title = browser.getTitle()
-      assert.strictEqual(title, 'WebDriverUniversity.com')
-  });
-});
